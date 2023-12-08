8efa58b231c9305d6dee819a2a3d28ef1b301866
DESKTOP-C5TOPPU\jason <jasonlee520888@gmail.com>
2023-12-09 02:23:09 +0800

    根据编码获得协议的配置。

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/service/V2RayTestService.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/service/V2RayTestService.kt
@@ -20,7 +20,7 @@ class V2RayTestService : Service() {
     override fun onCreate() {
         super.onCreate()
         Seq.setContext(this)
-        Libv2ray.initV2Env(Utils.userAssetPath(this), Utils.getDeviceIdForXUDPBaseKey())
+        Libv2ray.initV2Env(Utils.userAssetPath(this))
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
